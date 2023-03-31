a4d6e90a12d4630fc9329f13aa7c2311fe581168
T-Ishikawa-ishi <t.ishikawa@plakira.com>
2023-03-31 14:41:08 +0900

    Add name argument to hello.py

--- sampleproject/hello.py
+++ sampleproject/hello.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+import sys
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    name = sys.argv[1] if len(sys.argv) > 1 else "World"
+    greet(name)
